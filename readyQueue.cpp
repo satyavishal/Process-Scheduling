82a61f832ea44ac7462a857d1df1d5322ef34f07
Satya Vishal Thota <skillzworks.18@gmail.com>
2020-10-31 18:56:55 +0530

    Implemented the ReadyQueue

--- readyQueue.cpp
+++ readyQueue.cpp
@@ -1,3 +1,31 @@
 class ReadyQueue{
+private:
+  list<Job*> readyQueue[4];
+public:
+  void add_job(Job *j){
+    int c = j->getPriority() - 1;
+    readyQueue[c].push_back(j);
+  }
+
+  Job *extractJob(){
+    Job *j;
+    for(int i = 0;  i<4; i++){
+      if(!readyQueue[i].empty()){
+        j = readyQueue[i].front();
+        readyQueue[i].pop_front();
+        break;
+      }
+    }
+    return j;
+  }
+
+  int is_empty(){
+    for(int i=0;i<4;i++){
+      if(!readyQueue[i].empty()){
+        return 0;
+      }
+    }
+    return 1;
+  }
 
 };
